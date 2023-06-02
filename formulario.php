b57ef52b6220e9c52d1f0f471abfad2743d25967
lidasigas <123926811+lidasigas@users.noreply.github.com>
2023-06-02 14:39:50 +0200

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Formulario de Registro SCIII</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="group">
+
+        <form method="POST" action="">
+            <h2><em>Formulario de Registro</em></h2>
+            <label for="nombre"> Nombre <span><em>(requerido)</em></span></label>
+            <input type="text" name="nombre" class="form-input" required />
+
+            <label for="apellido">Apellido <span><em>(requerido)</em></span></label>
+            <input type="text" name="apellido" class="form-input" required />
+
+            <label for="email">Email <span><em>(requerido)</em></span></label>
+            <input type="email" name="email" class=form-input />
+
+            <input class="form-btn" name="submit" type="submit" value="Suscribirse" />
+
+
+            <?php
+
+            if ($_POST) {
+                $nombre = $_POST['nombre'];
+                $apellido = $_POST['apellido'];
+                $email = $_POST['email'];
+
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $bdname = "cursosql";
+
+                $conn = new mysqli($servername, $username, $password, $bdname);
+                if ($conn->connect_error) {
+                    die("Connection failed:" . $conn->connect_error);
+                }
+
+                $sql = "INSERT INTO usuarios (nombre, apellido, email)
+    VALUES ('$nombre', '$apellido', '$email')";
+
+                if ($conn->query($sql) == TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+
+                $conn->close();
+
+            }
+
+            ?>
+
+
+        </form>
+
+    </div>
+</body>
+
+</html>
